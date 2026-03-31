75f6d2b98275b686f56a885b82f5e1b8d9b27edd
sens <166984812+sensSD@users.noreply.github.com>
2026-03-31 13:41:51 +0800

    Update GitHub repository name in constants

--- src/consts.ts
+++ src/consts.ts
@@ -9,7 +9,7 @@ export const BLOG_SLUG_KEY = process.env.BLOG_SLUG_KEY || ''
  */
 export const GITHUB_CONFIG = {
 	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'sensSD',
-	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
+	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'blog-public',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
 	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '3192818',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
