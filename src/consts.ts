314454aedf526b868b5fe8ced92af2f4436b8616
sens <166984812+sensSD@users.noreply.github.com>
2026-03-31 13:33:06 +0800

    Update GITHUB_APP_ID constant value

--- src/consts.ts
+++ src/consts.ts
@@ -11,6 +11,6 @@ export const GITHUB_CONFIG = {
 	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'sens',
 	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
-	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '2301595',
+	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '3192818',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
 } as const
